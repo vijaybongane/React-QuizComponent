d6360c07dc5fd3f2541127ceefae3c9bca6b2f0f
vbongane <vbongane@salesforce.com>
2018-12-14 12:44:33 -0500

    completed module

--- src/Quiz.js
+++ src/Quiz.js
@@ -5,7 +5,7 @@ let quizData = require ('./quiz_data.json')
 class Quiz extends Component{
     constructor(props) {
         super(props)
-        this.state = { Quiz_position : 1 }
+        this.state = { quiz_position : 1 }
     }
 
     render() {
